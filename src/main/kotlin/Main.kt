ec81e33c8b7a465c9c889f29c3be95f241351fb2
Oreoluwa Akinwunmi <oreoluwaakinwunmi75@gmail.com>
2026-03-23 21:24:06 +0100

    Formatted the scores to 2 decimal places and also added a function to delete student record

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+// Using data class to store record
+
 data class Student(val name: String, val age: Int, val score: Double)
 
 val students = mutableListOf<Student>()
@@ -11,7 +13,8 @@ fun main() {
         println("2. Display All Students")
         println("3. Find Student by Name")
         println("4. Calculate Average Score")
-        println("5. Exit")
+        println("5. Delete Student Record")
+        println("6. Exit")
         print("Choose an option: ")
 
         when (readLine()) {
@@ -19,7 +22,8 @@ fun main() {
             "2" -> displayStudents()
             "3" -> findStudent()
             "4" -> calculateAverage()
-            "5" -> {
+            "5" -> deleteStudent()
+            "6" -> {
                 println("Goodbye")
                 running = false
             }
@@ -54,7 +58,7 @@ fun displayStudents() {
     if (students.isNotEmpty()) {
         println("--- All Students --")
         for (student in students) {
-            println("Name: ${student.name} \t Age: ${student.age} \t Score: ${student.score}") // make the displayed result be refined
+            println("Name: ${student.name} \t Age: ${student.age} \t Score: %.2f".format(student.score)) // make the displayed result be refined
         }
     } else {
         println("No student found. Please enter a student record")
@@ -69,10 +73,10 @@ fun findStudent() {
     val foundStudent = students.find {it.name.equals(searchName, true) }
 
     if (searchName.isNullOrBlank()) {
-        println("Invalid input")
+        println("Name cannot be empty.")
     } else if (foundStudent != null) {
         println("--- Found Student ---")
-        println("Name: ${foundStudent.name}, Age: ${foundStudent.age}, Score: ${foundStudent.score}") // make the displayed result be refined
+        println("Name: ${foundStudent.name}, Age: ${foundStudent.age}, Score: %.2f".format(foundStudent.score)) // make the displayed result be refined
     } else {
         println("Sorry, student record could not be found.")
     }
@@ -86,4 +90,31 @@ fun calculateAverage() {
         println("The average score is: %.2f".format(average)) // converts to 2 decimal places
     }
 
+}
+
+fun deleteStudent() {
+    println("Enter Student Name: ")
+    val searchName = readLine()?.trim()
+
+    if (searchName.isNullOrBlank()) {
+        println("Name cannot be empty.")
+        return
+    }
+
+    val delStudent = students.find { it.name.equals(searchName, ignoreCase = true) }
+
+    if (delStudent == null) {
+        println("Student not found.")
+    } else {
+        println("Name: ${delStudent.name}, Age: ${delStudent.age}, Score: %.2f".format(delStudent.score))
+        println("Are you sure you want to delete this record? y/n")
+        val confirmDelete = readLine()?.trim()
+
+        if (confirmDelete.equals("y", ignoreCase = true)) {
+            students.remove(delStudent)
+            println("Record deleted.")
+        } else if (confirmDelete.equals("n", ignoreCase = true)) {
+            println("Record not deleted.")
+        } else println("Invalid input.")
+    }
 }
